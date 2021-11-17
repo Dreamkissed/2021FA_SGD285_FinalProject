00be8f28f2178b1a069b10607acb7510d972824a
Dreamkissed <JennJP65@yahoo.com>
2021-11-17 16:42:25 -0500

    Enemy Code Start
    Starting AI and Controller for Enemies

---
+++ Assets/Scripts/Enemy/Enemy_AI.cs
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////
+//Project: MetroidVania
+//Name: Jennifer Wenner
+//Section: 2021FA.SGD.285.2141
+//Instructor: Prof. Wold
+//Date: 11/17/2021
+/////////////////////////////////////////////
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
