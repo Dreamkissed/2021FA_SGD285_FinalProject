b71d8bbd9a4936c1edb6be21581e287b0250939a
BigMike64 <kingmikeike2016@gmail.com>
2021-11-22 21:31:35 -0500

    Changes to level and character jump 1st
    Still working on the level

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -40,7 +40,7 @@ public class CharacterController : MonoBehaviour
     {
         if(Input.GetButtonDown("Jump") && isGrounded == true)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
             jumpSFX.Play();
         }
     }
