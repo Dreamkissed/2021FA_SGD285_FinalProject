0baa04e52908c2be620a030dc29cb42ac735bdd2
Zaclaine <zdlane@my.waketech.edu>
2021-11-29 15:47:52 -0500

    Changes

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterController : MonoBehaviour
 {
-
+    // PLAYER PROPERTIES
     public float moveSpeed = 5f;
     public bool isGrounded = false;
 
+    // HEALTH VALUES
+    private int playerHealth = 100;
+    private int playerDamage = 50;
+    private int enemyHealth = 50;
+    private int enemyDamage = 10;
+
+    // UI
+    public Text playerHealthText;
+    public GameObject gameOverPanel;
+
+    // AUDIO
+    public AudioSource gameOverSFX;
     public AudioSource jumpSFX;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOverPanel.SetActive(false);
+        playerHealthText.text = "Health: " + playerHealth.ToString();
     }
 
     // Update is called once per frame
@@ -34,6 +48,15 @@ public class CharacterController : MonoBehaviour
             characterScale.x = 1;
         }
         transform.localScale = characterScale;
+
+        GameOver();
+
+        // TESTING PURPOSES
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            playerHealth -= enemyDamage;
+            playerHealthText.text = "Health: " + playerHealth.ToString();
+        }
     }
 
     void Jump()
@@ -42,6 +65,25 @@ public class CharacterController : MonoBehaviour
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
             jumpSFX.Play();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            playerHealth -= enemyDamage;
+            playerHealthText.text = "Health: " + playerHealth.ToString();
+        }
+    }
+
+    void GameOver()
+    {
+        if (playerHealth <= 0)
+        {
+            gameOverPanel.SetActive(true);
+            gameOverSFX.Play();
+            gameOverSFX.Stop();
         }
     }
 }
